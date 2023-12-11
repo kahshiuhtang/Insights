49e79e3c1d8bdfece29a05b8fba1fb84bf66ac5b
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2023-12-10 19:41:33 -0500

    basic p2p setup

--- Insights/internal/network/client.go
+++ Insights/internal/network/client.go
@@ -5,10 +5,15 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"sync"
 
 	lib2p "github.com/libp2p/go-libp2p"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	net "github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/protocol"
+	drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
+	dutil "github.com/libp2p/go-libp2p/p2p/discovery/util"
 )
 func handleStream(stream net.Stream){
 	rw:= bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
@@ -51,6 +56,7 @@ func writeData(rw *bufio.ReadWriter){
 }
 func CreateClient(){
 	host, err := lib2p.New()
+	config, err := ParseFlags();
 	if err != nil {
 		fmt.Println("Error creating new");
 		panic(err)
@@ -62,7 +68,51 @@ func CreateClient(){
 	if err != nil{
 		panic(err)
 	}
+
+	//Bootstrap DHT, spawns background thread that refreshs peer table ~5 minutes
 	if err = dht.Bootstrap(ctx); err != nil{
 		panic(err)
 	}
+
+	//Connect to bootstrap nodes, tell us about other nodes in network
+	var wg sync.WaitGroup;
+	for _, peerAddr := range config.BootstrapPeers{
+		peerinfo, _ := peer.AddrInfoFromP2pAddr(peerAddr);
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err :=  host.Connect(ctx, *peerinfo); err != nil{
+				fmt.Println(err);
+			}else{
+				fmt.Println("Connection established with bootstrap node")
+			}
+		}()
 	}
+	wg.Wait();
+	
+	//Rendezvous for others
+	routingDiscovery := drouting.NewRoutingDiscovery(dht)
+	dutil.Advertise(ctx, routingDiscovery, config.RendezvousString)
+
+	// Finding other hosts who have also announced
+	peerChan, err := routingDiscovery.FindPeers(ctx, config.RendezvousString);
+
+	if err != nil{
+		panic(err);
+	}
+	for peer := range peerChan {
+		if peer.ID == host.ID(){
+			continue
+		}
+		stream, err := host.NewStream(ctx, peer.ID, protocol.ID(config.ProtocolID))
+
+		if err != nil {
+			continue;
+		}else{
+			rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream));
+			go writeData(rw);
+			go readData(rw);
+		}
+		
+	}
+}
