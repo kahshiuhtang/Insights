06af9de1150b7327f08a7ba2939860cefb5f9911
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2023-12-15 16:48:59 -0500

    deleted blockchain

--- Insights/cmd/Insights/main.go
+++ Insights/cmd/Insights/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"net"
+	"sync"
 
-	"github.com/kahshiuhtang/Insights/internal/blockchain"
 	"github.com/kahshiuhtang/Insights/internal/network"
 )
-
 func main() {
     fmt.Println("Hello, Go!")
-	blockchain.CreateBlockchain(5);
-	network.CreateClient();
+	var action string;
+	flag.StringVar(&action, "action", "create", "Specify an Action");
+	flag.Parse();
+	fmt.Println("Action", action);
+	var wg sync.WaitGroup;
+	serv := network.CreateServer();
+	go serv.Start(&wg);
+	wg.Add(1);
+	wg.Add(1)
+	conn, err := net.Dial("tcp", "localhost:8081");
+	if err != nil{
+		return;
+	}
+	_, err = conn.Write([]byte("Hello, server!"));
+	if err != nil{
+		return
+	}
+	wg.Wait();
 }
--- Insights/internal/network/server.go
+++ Insights/internal/network/server.go
@@ -1,1 +1,48 @@
 package network
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+type Server struct{
+
+}
+
+func CreateServer() Server{
+	return Server{};
+}
+func (s Server) Start(wg *sync.WaitGroup){
+	ln, err := net.Listen("tcp", ":8081");
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil{
+			fmt.Println(err)
+			continue
+		}
+
+		go handleConnection(conn, wg);
+	}
+
+}
+func handleConnection(conn net.Conn, wg *sync.WaitGroup) {
+    // Close the connection when we're done
+    defer conn.Close()
+
+    // Read incoming data
+    buf := make([]byte, 1024)
+    _, err := conn.Read(buf)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    // Print the incoming data
+    fmt.Printf("Received: %s", buf)
+	wg.Done();
+}
